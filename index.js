23f7a3b0b61814c6916d43a5e529e6d5d4d65646
denimar <denimar@gmail.com>
2018-06-17 15:57:32 -0300

    implemented version 1.0.0.12

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
 
   app.get('/', (req, res) => {
-    res.end('Hello Denimar!! Microservice01... my version 1.0.0.11')
+    res.end('Hello Denimar!! Microservice01... my version 1.0.0.12')
   })
 
   console.log(`Listening on port ${PORT}`)
