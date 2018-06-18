b53732abb0aea7eb95cf678c147a61352caddc3d
denimar <denimar@gmail.com>
2018-06-18 06:43:02 -0300

    fixed the errors

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ describe('addTwoNumbers()', function () {
     var sum1 = x + y;
 
     // 2. ACT
-    var sum2 = addTwoNumbers(62, y);
+    var sum2 = addTwoNumbers(x, y);
 
     // 3. ASSERT
     expect(sum2).to.be.equal(sum1);
